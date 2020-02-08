1ba1c4fb1112ba7e3ab76a1be526715c608f4c37
heyongjian <hyjfine@gmail.com>
2020-02-08 22:02:30 +0800

    update to androidX

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/hyjfine/Sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/hyjfine/Play/flutter/flutter_redux_sample"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/hyjfine/Sdk/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
